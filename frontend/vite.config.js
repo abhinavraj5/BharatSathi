93c08b8b94e0815f2e04812b323a4aaad8b0ce5e
abhinavraj5 <abhinavkvo10@gmail.com>
2026-04-23 22:58:47 +0530

    video2

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import fs from 'fs'
 
 export default defineConfig({
   plugins: [react()],
-  server: { port: 5173 }
-});
+  server: {
+    host: true,
+    https: {
+      key: fs.readFileSync('./192.168.9.122-key.pem'),
+      cert: fs.readFileSync('./192.168.9.122.pem'),
+    }
+  }
+})
